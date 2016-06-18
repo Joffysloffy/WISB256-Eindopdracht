566dcae8e56cda7f9d7b05c371782a92ab44b40b
joffysloffy <joost.franssen@hotmail.com>
2016-06-18 15:17:02 +0200

    Added support for most common types of simplifications for operations. Bug fixes.

--- Expressions.py
+++ Expressions.py
@@ -85,6 +85,11 @@ class Expression:
                      "/": associativity(left=True, right=False),
                      "**": associativity(left=False, right=True),
                      "~": associativity(left=False, right=True)}
+    COMMUTATIVITY = {"+": True,
+                     "-": False,
+                     "*": True,
+                     "/": False,
+                     "**": False}
     PRECEDENCE = {"+": 0,
                   "-": 0,
                   "*": 1,
@@ -117,7 +122,7 @@ class Expression:
 
     # the same as negation ~ so that overloaded negation with - be possible
     def __neg__(self):
-        return NegationNode(self)
+        return ~self
 
     # basic Shunting-yard algorithm
     @staticmethod
@@ -127,10 +132,10 @@ class Expression:
         # stack used by the Shunting-Yard algorithm
         stack = []
         # output of the algorithm: a list representing the formula in RPN
-        # this will contain Constant's and '+'s
+        # this will contain instances of subclasses of Expression
         output = []
 
-        # list of operators
+        # list of operators and functions
         oplist = Expression.OPERATOR_SYMBOLS
         fnlist = list(Function.BUILTIN_FUNCTIONS.keys())
 
@@ -232,10 +237,13 @@ class Expression:
     #     a tuple with first an Expression and then a list of variables used
     #     just an Expression, provided it use FunctionBase.VAR as its variables
     def substitute(self, substitutions_unknowns, find_derivatives=True):
-        pass
+        return self
 
     def derivative(self, variable):
         pass
+
+    def simplify(self):
+        return self
 
     def __contains__(self, item):
         if item == self:
@@ -260,11 +268,14 @@ class Constant(Expression):
     def evaluate(self, substitutions_unknowns={}):
         return self.value
 
-    def substitute(self, substitutions_unknowns, find_derivatives=True):
-        return self
-
     def derivative(self, variable):
         return Constant(0)
+
+    def simplify(self):
+        if self.value < 0:
+            return -Constant(-self.value)
+        else:
+            return self
 
     def __contains__(self, item):
         return self == item
@@ -404,7 +415,7 @@ class Function(Expression):
         try:
             # allow for the use of the default variables VAR
             val = substitutions_unknowns[self.base.symbol]
-            if isinstance(tuple):
+            if isinstance(val, tuple):
                 (f, variables) = val
             else:
                 f = val
@@ -479,6 +490,9 @@ class Function(Expression):
         else:
             return Constant(0)
 
+    def simplify(self):
+        return Function(self.base, *[a.simplify() for a in self.arguments])
+
     def __contains__(self, item):
         if item == self:
             return True
@@ -536,6 +550,10 @@ class UnaryNode(OperatorNode):
         der = self.operand.derivative(variable)
         return eval("%sder" % self.op_symbol)
 
+    def simplify(self):
+        operand = self.operand.simplify()
+        return eval("%soperand" % self.op_symbol)
+
     def __contains__(self, item):
         if item == self:
             return True
@@ -563,14 +581,28 @@ class NegationNode(UnaryNode):
         der = self.operand.derivative(variable)
         return eval("-der")
 
+    def simplify(self):
+        operand = self.operand.simplify()
+
+        # undo double negation
+        if isinstance(operand, NegationNode):
+            return operand.operand
+
+        # -0 is 0
+        if operand == Constant(0):
+            return Constant(0)
+
+        return super().simplify()
+
 
 class BinaryNode(OperatorNode):
     """A node in the expression tree representing a binary operator."""
 
-    def __init__(self, lhs: Expression, rhs: Expression, op_symbol):
+    def __init__(self, lhs: Expression, rhs: Expression, op_symbol: str, is_commutative):
         super().__init__(op_symbol, Expression.ASSOCIATIVITY[op_symbol].left, Expression.ASSOCIATIVITY[op_symbol].right, Expression.PRECEDENCE[op_symbol])
         self.lhs = lhs
         self.rhs = rhs
+        self.is_commutative = is_commutative
 
     def __eq__(self, other):
         if type(self) == type(other):
@@ -611,6 +643,77 @@ class BinaryNode(OperatorNode):
         rvalue = self.rhs.derivative(variable)
         return eval("lvalue %s rvalue" % self.op_symbol)
 
+    def simplify(self):
+        lhs = self.lhs.simplify()
+        rhs = self.rhs.simplify()
+
+        # evaluate operations between two constants
+        if isinstance(lhs, Constant) and isinstance(rhs, Constant):
+            return Constant(eval("%s %s %s" % (lhs.value, self.op_symbol, rhs.value)))
+
+        # attempt to simplify consecutive operations using commutativity by cycling the operands
+        # e.g., if a*b*c does not simplify, try a*c*b and then a*(b*c)
+        if self.is_commutative:
+            # cycling in case of lhs
+            if type(self) == type(lhs):
+                operandslr = eval("lhs.lhs %s rhs" % self.op_symbol)
+                operandslr_simpl = operandslr.simplify()
+                if operandslr == operandslr_simpl:
+                    operandsrr = eval("lhs.rhs %s rhs" % self.op_symbol)
+                    operandsrr_simpl = operandsrr.simplify()
+                    if not operandsrr == operandsrr_simpl:
+                        return eval("lhs.lhs %s operandsrr_simpl" % self.op_symbol)
+                else:
+                    return eval("operandslr_simpl %s lhs.rhs" % self.op_symbol)
+            # cycling in case of rhs reduces to the case of lhs
+            if type(self) == type(rhs):
+                operandsll = eval("lhs %s rhs.lhs" % self.op_symbol)
+                operandsll_simpl = operandsll.simplify()
+                if operandsll == operandsll_simpl:
+                    operandslr = eval("lhs %s rhs.rhs" % self.op_symbol)
+                    operandslr_simpl = operandslr.simplify()
+                    if not operandslr == operandslr_simpl:
+                        return eval("operandslr_simpl %s rhs.lhs" % self.op_symbol)
+                else:
+                    return eval("operandsll_simpl %s rhs.rhs" % self.op_symbol)
+
+        # if all else fails, return the operation between the simplified sides
+        return eval("lhs %s rhs" % self.op_symbol)
+
+    @staticmethod
+    # distributive laws, e.g., a*b+a*c = a*(b+c)
+    # e.g., node_lower is AdditionNode and node_higher is MultiplicationNode
+    # left and right represent left- and right-distributivity
+    # returns None if no simplification was made
+    def simplify_distributively(lhs, rhs, node_lower, node_higher, left=True, right=True):
+        # if lhs is the higher precedence node
+        if isinstance(lhs, node_higher):
+            # if also rhs is higher precedence node
+            if isinstance(rhs, node_higher):
+                if left:
+                    if rhs.lhs == lhs.lhs:
+                        return node_higher(rhs.lhs, node_lower(lhs.rhs, rhs.rhs)).simplify()
+                    if lhs.is_commutative:
+                        if rhs.lhs == lhs.rhs:
+                            return node_higher(rhs.lhs, node_lower(lhs.lhs, rhs.rhs)).simplify()
+                if right:
+                    if lhs.is_commutative:
+                        if rhs.rhs == lhs.lhs:
+                            return node_higher(node_lower(lhs.rhs, rhs.lhs), rhs.rhs).simplify()
+                    if rhs.rhs == lhs.rhs:
+                        return node_higher(node_lower(lhs.lhs, rhs.lhs), rhs.rhs).simplify()
+            else:
+                if lhs.lhs == rhs and left:
+                    return node_higher(rhs, node_lower(lhs.rhs, Constant(1))).simplify()
+                elif lhs.rhs == rhs and right:
+                    return node_higher(node_lower(lhs.lhs, Constant(1)), rhs).simplify()
+        # if just rhs is higher precedence node
+        if isinstance(rhs, node_higher):
+            if rhs.lhs == lhs and left:
+                return node_higher(lhs, node_lower(Constant(1), rhs.rhs)).simplify()
+            elif rhs.rhs == lhs and right:
+                return node_higher(node_lower(Constant(1), rhs.lhs), lhs).simplify()
+
     def __contains__(self, item):
         if item == self:
             return True
@@ -621,21 +724,82 @@ class AdditionNode(BinaryNode):
     """Represents the addition operator"""
 
     def __init__(self, lhs, rhs):
-        super().__init__(lhs, rhs, Expression.OPERATOR_LIST["Addition"])
+        op_symbol = Expression.OPERATOR_LIST["Addition"]
+        super().__init__(lhs, rhs, op_symbol, Expression.COMMUTATIVITY[op_symbol])
+
+    def simplify(self):
+        lhs = self.lhs.simplify()
+        rhs = self.rhs.simplify()
+
+        # adding a negative is subtraction
+        if isinstance(rhs, NegationNode):
+            return (lhs - rhs.operand).simplify()
+
+        # 0 is a unit w.r.t +
+        if lhs == Constant(0):
+            return rhs
+        if rhs == Constant(0):
+            return lhs
+
+        # adding two of the same elements is multiplication by 2
+        if lhs == rhs:
+            return (Constant(2) * lhs).simplify()
+
+        distr_simpl = BinaryNode.simplify_distributively(lhs, rhs, AdditionNode, MultiplicationNode)
+        if distr_simpl is not None:
+            return distr_simpl
+
+        if isinstance(lhs, DivisionNode) and isinstance(rhs, DivisionNode):
+            if lhs.rhs == rhs.rhs:
+                return ((lhs.lhs + rhs.lhs) / lhs.rhs).simplify()
+
+        return super().simplify()
 
 
 class SubtractionNode(BinaryNode):
     """Represents the subtraction operator"""
 
     def __init__(self, lhs, rhs):
-        super().__init__(lhs, rhs, Expression.OPERATOR_LIST["Subtraction"])
+        op_symbol = Expression.OPERATOR_LIST["Subtraction"]
+        super().__init__(lhs, rhs, op_symbol, Expression.COMMUTATIVITY[op_symbol])
+
+    def simplify(self):
+        lhs = self.lhs.simplify()
+        rhs = self.rhs.simplify()
+
+        # subtracting a negative is addition
+        if isinstance(rhs, NegationNode):
+            return (lhs + rhs.operand).simplify()
+
+        # subtracting from 0 is negation
+        if lhs == Constant(0):
+            return (-rhs).simplify()
+
+        # subtracting 0 does nothing
+        if rhs == Constant(0):
+            return lhs
+
+        # subtracting the same elements is 0
+        if rhs == lhs:
+            return Constant(0)
+
+        distr_simpl = BinaryNode.simplify_distributively(lhs, rhs, SubtractionNode, MultiplicationNode)
+        if distr_simpl is not None:
+            return distr_simpl
+
+        if isinstance(lhs, DivisionNode) and isinstance(rhs, DivisionNode):
+            if lhs.rhs == rhs.rhs:
+                return ((lhs.lhs - rhs.lhs) / lhs.rhs).simplify()
+
+        return super().simplify()
 
 
 class MultiplicationNode(BinaryNode):
     """Represents the multiplication operator"""
 
     def __init__(self, lhs, rhs):
-        super().__init__(lhs, rhs, Expression.OPERATOR_LIST["Multiplication"])
+        op_symbol = Expression.OPERATOR_LIST["Multiplication"]
+        super().__init__(lhs, rhs, op_symbol, Expression.COMMUTATIVITY[op_symbol])
 
     def derivative(self, variable):
         if isinstance(variable, str):
@@ -645,12 +809,54 @@ class MultiplicationNode(BinaryNode):
         rderiv = self.rhs.derivative(variable)
         return lderiv * self.rhs + self.lhs * rderiv
 
+    def simplify(self):
+        lhs = self.lhs.simplify()
+        rhs = self.rhs.simplify()
+
+        # multiplication with 0 is 0
+        if lhs == Constant(0) or rhs == Constant(0):
+            return Constant(0)
+
+        # 1 is a unit w.r.t. *
+        if lhs == Constant(1):
+            return rhs
+        if rhs == Constant(1):
+            return lhs
+
+        # take negation outside of multiplication to simplify with other operators
+        if isinstance(lhs, NegationNode):
+            if isinstance(rhs, NegationNode):
+                # multiplying two negatives yields positives
+                return (lhs.operand * rhs.operand).simplify()
+            else:
+                return (-(lhs.operand * rhs)).simplify()
+        elif isinstance(rhs, NegationNode):
+            return (-(lhs * rhs.operand)).simplify()
+
+        # multiplication with a fraction should be multiplied with the numerator for further simplification (e.g., 2*1/3 -> 2/3)
+        if isinstance(lhs, DivisionNode):
+            return ((lhs.lhs * rhs) / lhs.rhs).simplify()
+        if isinstance(rhs, DivisionNode):
+            return ((lhs * rhs.lhs) / rhs.rhs).simplify()
+
+        # multiplying the same thing is squaring
+        if lhs == rhs:
+            return (lhs ** Constant(2)).simplify()
+
+        # product of two powers adds the powers
+        distr_simpl = BinaryNode.simplify_distributively(lhs, rhs, AdditionNode, PowerNode, True, False)
+        if distr_simpl is not None:
+            return distr_simpl
+
+        return super().simplify()
+
 
 class DivisionNode(BinaryNode):
     """Represents the division operator"""
 
     def __init__(self, lhs, rhs):
-        super().__init__(lhs, rhs, Expression.OPERATOR_LIST["Division"])
+        op_symbol = Expression.OPERATOR_LIST["Division"]
+        super().__init__(lhs, rhs, op_symbol, Expression.COMMUTATIVITY[op_symbol])
 
     def derivative(self, variable: Variable):
         if isinstance(variable, str):
@@ -658,14 +864,70 @@ class DivisionNode(BinaryNode):
 
         lderiv = self.lhs.derivative(variable)
         rderiv = self.rhs.derivative(variable)
-        return lderiv * self.rhs - self.lhs * rderiv / self.rhs ** Constant(2)
+        return (lderiv * self.rhs - self.lhs * rderiv) / self.rhs ** Constant(2)
+
+    def simplify(self):
+        lhs = self.lhs.simplify()
+        rhs = self.rhs.simplify()
+
+        # dividing by 0 is impossible
+        if rhs == Constant(0):
+            raise ZeroDivisionError()
+
+        # 0 divided by anything non-zero is 0
+        if lhs == Constant(0):
+            return Constant(0)
+
+        # dividing by 1 does nothing
+        if rhs == Constant(1):
+            return lhs
+
+        # do not evaluate division by two integers (as in super().simplify()) but simplify the fraction
+        if isinstance(lhs, Constant) and isinstance(rhs, Constant):
+            if isinstance(lhs.value, int) and isinstance(rhs.value, int):
+                gcd = math.gcd(lhs.value, rhs.value)
+                if gcd > 1:
+                    return (Constant(lhs.value // gcd) / Constant(rhs.value // gcd)).simplify()
+                else:
+                    return lhs / rhs
+
+        # take negation outside of a fraction to simplify with other operators
+        if isinstance(lhs, NegationNode):
+            if isinstance(rhs, NegationNode):
+                # divide two negatives yields positives
+                return (lhs.operand / rhs.operand).simplify()
+            else:
+                return (-(lhs.operand / rhs)).simplify()
+        elif isinstance(rhs, NegationNode):
+            return (-(lhs / rhs.operand)).simplify()
+
+        # dividing by a fraction is multiplication with the reciprocal
+        if isinstance(rhs, DivisionNode):
+            return (lhs * rhs.rhs / rhs.lhs).simplify()
+
+        # dividing a fraction is multiplication with its denominator
+        if isinstance(lhs, DivisionNode):
+            return lhs.lhs / (lhs.rhs * rhs).simplify()
+
+        # dividing by a negative power is multiplying with a positive one
+        if isinstance(rhs, PowerNode):
+            if isinstance(rhs.rhs, NegationNode):
+                return (lhs * (rhs.lhs ** rhs.rhs.operand)).simplify()
+
+        # division of two powers adds the powers
+        distr_simpl = BinaryNode.simplify_distributively(lhs, rhs, SubtractionNode, PowerNode, True, False)
+        if distr_simpl is not None:
+            return distr_simpl
+
+        return super().simplify()
 
 
 class PowerNode(BinaryNode):
     """Represents the power operator"""
 
     def __init__(self, lhs, rhs):
-        super().__init__(lhs, rhs, Expression.OPERATOR_LIST["Power"])
+        op_symbol = Expression.OPERATOR_LIST["Power"]
+        super().__init__(lhs, rhs, op_symbol, Expression.COMMUTATIVITY[op_symbol])
 
     def derivative(self, variable):
         if isinstance(variable, str):
@@ -678,6 +940,39 @@ class PowerNode(BinaryNode):
             return self * Function("log", self.lhs) * self.rhs.derivative(variable)
 
         return self * (self.rhs.derivative(variable) * Function("log", self.lhs) + self.rhs * self.lhs.derivative(variable) / self.lhs)
+
+    def simplify(self):
+        lhs = self.lhs.simplify()
+        rhs = self.rhs.simplify()
+
+        # raising to the 0-th power is 1 (Python evaluates 0**0 as 1)
+        if rhs == Constant(0):
+            return Constant(1)
+
+        # raising 0 or 1 to any power remains 0 or 1 respectively; raising to the power 1 does nothing
+        if lhs == Constant(0) or lhs == Constant(1) or rhs == Constant(1):
+            return lhs
+
+        #squaring is the inverse of taking the square root
+        if rhs == Constant(2):
+            if isinstance(lhs, Function):
+                if lhs.base.symbol == "sqrt":
+                    return lhs.arguments[0].simplify()
+
+        # a power of a power is the product of the powers (with some adjustments for powers of negatives)
+        if isinstance(lhs, PowerNode):
+            power = (lhs.rhs * rhs).simplify()
+            base = lhs.lhs
+            if isinstance(base, NegationNode):
+                if isinstance(lhs.rhs, Constant):
+                    if float(lhs.rhs).is_integer():
+                        if int(lhs.rhs.value) % 2 == 0:
+                            base = base.operand
+                        return (base ** power).simplify()
+            else:
+                return (base ** power).simplify()
+
+        return super().simplify()
 
 
 Function.BUILTIN_FUNCTIONS = {"sin": FunctionBase("sin", math.sin, Function("cos")),
@@ -700,8 +995,7 @@ Function.BUILTIN_FUNCTIONS = {"sin": FunctionBase("sin", math.sin, Function("cos
                               "floor": FunctionBase("floor", math.floor),
                               "factorial": FunctionBase("factorial", math.factorial),
                               "abs": FunctionBase("abs", math.fabs),
-                              "sqrt": FunctionBase("sqrt", math.sqrt, Constant(1) / (Constant(2) * Function("sqrt")))
+                              "sqrt": FunctionBase("sqrt", math.sqrt, Constant(1) / (Constant(2) * Function("sqrt"))),
                               }
-
 
 
